6a2c3bed29bb5c3e44f517ccf28275329987c798
constellation39 <constellation39@outlook.com>
2024-12-24 16:03:44 +0800

    refactor: simplify event handling and add debug logging for unknown events

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from pkg.plugin.context import register, handler, llm_func, BasePlugin, APIHost, EventContext
-from pkg.plugin.events import *  # 导入事件类
+from pkg.plugin.context import APIHost, BasePlugin, EventContext, handler, register
+from pkg.plugin.events import (
+    GroupNormalMessageReceived,
+    PersonNormalMessageReceived,
+)
 
 
 # 注册插件
 @register(name="Hello", description="hello world", version="0.1", author="RockChinQ")
 class MyPlugin(BasePlugin):
-
     # 插件加载时触发
     def __init__(self, host: APIHost):
         pass
@@ -17,32 +19,43 @@ class MyPlugin(BasePlugin):
     # 当收到个人消息时触发
     @handler(PersonNormalMessageReceived)
     async def person_normal_message_received(self, ctx: EventContext):
-        msg = ctx.event.text_message  # 这里的 event 即为 PersonNormalMessageReceived 的对象
-        if msg == "hello":  # 如果消息为hello
+        match event := ctx.event:
+            case PersonNormalMessageReceived():
+                msg = (
+                    event.text_message
+                )  # 这里的 event 即为 PersonNormalMessageReceived 的对象
+                if msg == " ":  # 如果消息为hello
+                    # 输出调试信息
+                    self.ap.logger.debug("hello, {}".format(event.sender_id))
 
-            # 输出调试信息
-            self.ap.logger.debug("hello, {}".format(ctx.event.sender_id))
+                    # 回复消息 "hello, <发送者id>!"
+                    ctx.add_return("reply", ["hello, {}!".format(event.sender_id)])
 
-            # 回复消息 "hello, <发送者id>!"
-            ctx.add_return("reply", ["hello, {}!".format(ctx.event.sender_id)])
-
-            # 阻止该事件默认行为（向接口获取回复）
-            ctx.prevent_default()
+                    # 阻止该事件默认行为（向接口获取回复）
+                    ctx.prevent_default()
+            case _:
+                self.ap.logger.debug("unknown event: {}".format(event))
+                pass
 
     # 当收到群消息时触发
     @handler(GroupNormalMessageReceived)
     async def group_normal_message_received(self, ctx: EventContext):
-        msg = ctx.event.text_message  # 这里的 event 即为 GroupNormalMessageReceived 的对象
-        if msg == "hello":  # 如果消息为hello
+        match event := ctx.event:
+            case GroupNormalMessageReceived():
+                msg = event.text_message
+                if msg == "hello":  # 如果消息为hello
+                    # 输出调试信息
+                    self.ap.logger.debug("hello, {}".format(event.sender_id))
 
-            # 输出调试信息
-            self.ap.logger.debug("hello, {}".format(ctx.event.sender_id))
+                    # 回复消息 "hello, everyone!"
+                    ctx.add_return("reply", ["hello, everyone!"])
 
-            # 回复消息 "hello, everyone!"
-            ctx.add_return("reply", ["hello, everyone!"])
+                    # 阻止该事件默认行为（向接口获取回复）
+                    ctx.prevent_default()
 
-            # 阻止该事件默认行为（向接口获取回复）
-            ctx.prevent_default()
+            case _:
+                self.ap.logger.debug("unknown event: {}".format(event))
+                pass
 
     # 插件卸载时触发
     def __del__(self):
